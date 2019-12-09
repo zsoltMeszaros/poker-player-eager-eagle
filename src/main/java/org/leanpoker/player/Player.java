87adb5950d119e7325605bf4321f961065fae927
Adam Landfrasz <adam.landfrasz@gmail.com>
2019-12-09 13:31:51 +0100

    new version

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -8,7 +8,7 @@ public class Player {
 
     private static Gson gson = new Gson();
 
-    static final String VERSION = "1.2.0";
+    static final String VERSION = "1.2.2a";
 
     public static int betRequest(JsonElement request) {
 
