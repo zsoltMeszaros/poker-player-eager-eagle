0f44c45bf576cf65e9d6a5f83d6385daee0fc317
zsoltMeszaros <meszazaolt13@gmail.com>
2019-12-09 10:48:17 +0100

    json management

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,20 +1,74 @@
 package org.leanpoker.player;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class Player {
 
-    static final String VERSION = "init 1.0 commit almafa";
+    private static Gson gson = new Gson();
 
-
+    static final String VERSION = "1.1";
 
     public static int betRequest(JsonElement request) {
 
-        System.out.println(request);
+        JsonObject jsonObject = request.getAsJsonObject();
+
+        int inAction = jsonObject.get("in_action").getAsInt();
+
+
+        JsonElement self = jsonObject.get("players").getAsJsonArray().get(inAction);
+
+
+        System.out.println(self);
 
         return 0;
+    }
+
+    public static void main(String[] args) {
+
+        String jsonString = "{\n" +
+                "  \"players\":[\n" +
+                "    {\n" +
+                "      \"name\":\"Player 1\",\n" +
+                "      \"stack\":1000,\n" +
+                "      \"status\":\"active\",\n" +
+                "      \"bet\":0,\n" +
+                "      \"hole_cards\":[],\n" +
+                "      \"version\":\"Version name 1\",\n" +
+                "      \"id\":0\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"name\":\"Player 2\",\n" +
+                "      \"stack\":1000,\n" +
+                "      \"status\":\"active\",\n" +
+                "      \"bet\":0,\n" +
+                "      \"hole_cards\":[],\n" +
+                "      \"version\":\"Version name 2\",\n" +
+                "      \"id\":1\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"tournament_id\":\"550d1d68cd7bd10003000003\",\n" +
+                "  \"game_id\":\"550da1cb2d909006e90004b1\",\n" +
+                "  \"round\":0,\n" +
+                "  \"bet_index\":0,\n" +
+                "  \"small_blind\":10,\n" +
+                "  \"orbits\":0,\n" +
+                "  \"dealer\":0,\n" +
+                "  \"community_cards\":[],\n" +
+                "  \"current_buy_in\":0,\n" +
+                "  \"pot\":0\n" +
+                "}";
+
+
+        Player.betRequest(new JsonParser().parse(jsonString));
+
+
     }
 
     public static void showdown(JsonElement game) {
