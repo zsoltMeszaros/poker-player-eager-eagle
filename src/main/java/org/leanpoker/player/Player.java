5f7db3279fb32992a70da6a8226900d0564097c2
Adam Landfrasz <adam.landfrasz@gmail.com>
2019-12-09 13:32:35 +0100

    switch case hotfix

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -8,7 +8,7 @@ public class Player {
 
     private static Gson gson = new Gson();
 
-    static final String VERSION = "1.2.2a";
+    static final String VERSION = "1.2.2b";
 
     public static int betRequest(JsonElement request) {
 
@@ -95,6 +95,7 @@ public class Player {
                     break;
                 case "A":
                     value = 14;
+                    break;
                 default:
                     value = card.get("rank").getAsInt();
             }
